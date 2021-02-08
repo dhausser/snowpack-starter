bc0950a3101ebc73211cedf6ab47a66c767fb113
Davy Hausser <davy.hausser@gmail.com>
2021-02-08 15:44:36 +0100

    TypeScript initial setup

--- snowpack.config.js
+++ snowpack.config.js
@@ -7,7 +7,11 @@ module.exports = {
     public: { url: "/", static: true },
     src: { url: "/dist" },
   },
-  plugins: ["@snowpack/plugin-react-refresh", "@snowpack/plugin-dotenv"],
+  plugins: [
+    "@snowpack/plugin-react-refresh",
+    "@snowpack/plugin-dotenv",
+    "@snowpack/plugin-typescript",
+  ],
   packageOptions: {
     /* ... */
   },
