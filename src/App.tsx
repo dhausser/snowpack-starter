cc247afdfe900e01a21bf4b056e6323a3588f181
Davy Hausser <davy.hausser@gmail.com>
2021-02-22 13:59:24 +0100

    replaced useAsync by useQuery from react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,63 +1,17 @@
 import React from 'react'
-import {client} from './utils/api-client'
-import {useAsync} from './utils/hooks'
+import {QueryClient, QueryClientProvider} from 'react-query'
+import {User} from './components/user'
 
 interface AppProps {}
 
-async function getUser() {
-  const data = await client()
-
-  if (data) {
-    const [user] = data.results
-    return user
-  }
-
-  return null
-}
+const queryClient = new QueryClient()
 
 function App(props: AppProps) {
-  const {
-    data: user,
-    error,
-    isLoading,
-    isIdle,
-    isError,
-    isSuccess,
-    run,
-    setData,
-  } = useAsync()
-
-  React.useEffect(() => {
-    run(getUser())
-  }, [run])
-
-  if (isLoading || isIdle) {
-    return <p>Loading...</p>
-  }
-
-  if (isError) {
-    return <pre>{error.message}</pre>
-  }
-
-  if (isSuccess) {
-    return user ? (
-      <div>
-        <h1>
-          {user.name.first} {user.name.last}
-        </h1>
-        <pre>{user.email}</pre>
-        <p>
-          From {user.location.city}, {user.location.country}
-        </p>
-        <img src={user.picture.large}></img>
-        <pre>{JSON.stringify(user, null, 2)}</pre>
-      </div>
-    ) : (
-      <h1>Welcome to Snowpack!</h1>
-    )
-  }
-
-  return null
+  return (
+    <QueryClientProvider client={queryClient}>
+      <User />
+    </QueryClientProvider>
+  )
 }
 
 export default App
