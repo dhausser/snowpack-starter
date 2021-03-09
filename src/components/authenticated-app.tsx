c87d549b4124a6fde02eda48f8356f91b653b17b
Davy Hausser <davy.hausser@gmail.com>
2021-03-09 10:28:18 +0100

    authentication started

---
+++ src/components/authenticated-app.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+function AuthenticatedApp() {
+  return <p>authenticated</p>
+}
+
+export {AuthenticatedApp}
---
+++ src/components/unauthenticated-app.tsx
@@ -0,0 +1,7 @@
+import * as React from 'react'
+
+function UnauthenticatedApp() {
+  return <p>unauthenticated</p>
+}
+
+export {UnauthenticatedApp}
