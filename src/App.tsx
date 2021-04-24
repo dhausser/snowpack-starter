953894199ed079e165c0161daa5c300fb6ce04c1
Davy Hausser <davy.hausser@gmail.com>
2021-04-24 12:01:30 +0200

    modal form

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import React from 'react'
-import { QueryClient, QueryClientProvider } from 'react-query'
-import { Form } from './components/form'
-
-const queryClient = new QueryClient()
+import React, { useState } from 'react'
+import { FormDemo } from './components/form'
+import { Modal, Button } from 'antd'
 
 function App() {
+  const [isModalVisible, setIsModalVisible] = useState(true)
+
+  const showModal = () => {
+    setIsModalVisible(true)
+  }
+
+  const handleOk = () => {
+    setIsModalVisible(false)
+  }
+
+  const handleCancel = () => {
+    setIsModalVisible(false)
+  }
+
   return (
-    <QueryClientProvider client={queryClient}>
-      <Form />
-    </QueryClientProvider>
+    <div className="container">
+      <Button type="primary" onClick={showModal}>
+        Open Modal
+      </Button>
+      <Modal
+        title="Basic Modal"
+        visible={isModalVisible}
+        onOk={handleOk}
+        onCancel={handleCancel}
+      >
+        <FormDemo />
+      </Modal>
+    </div>
   )
 }
 
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useForm } from 'react-hook-form'
+import { Form, Input, Button, Checkbox } from 'antd'
 
 enum GenderEnum {
   female = 'female',
@@ -12,13 +13,60 @@ interface IFormInput {
   gender: GenderEnum
 }
 
-export function Form() {
+const layout = {
+  labelCol: { span: 8 },
+  wrapperCol: { span: 16 },
+}
+const tailLayout = {
+  wrapperCol: { offset: 8, span: 16 },
+}
+
+export function FormDemo() {
   const { register, handleSubmit } = useForm<IFormInput>()
-  const onSubmit = (data: IFormInput) => console.log(data)
+  // const onSubmit = (data: IFormInput) => console.log(data)
+
+  const onFinish = (values: any) => {
+    console.log('Success:', values)
+  }
+
+  const onFinishFailed = (errorInfo: any) => {
+    console.log('Failed:', errorInfo)
+  }
 
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <label>First Name</label>
+    <Form
+      {...layout}
+      name="basic"
+      initialValues={{ remember: true }}
+      onFinish={onFinish}
+      onFinishFailed={onFinishFailed}
+    >
+      <Form.Item
+        label="Username"
+        name="username"
+        rules={[{ required: true, message: 'Please input your username!' }]}
+      >
+        <Input />
+      </Form.Item>
+
+      <Form.Item
+        label="Password"
+        name="password"
+        rules={[{ required: true, message: 'Please input your password!' }]}
+      >
+        <Input.Password />
+      </Form.Item>
+
+      <Form.Item {...tailLayout} name="remember" valuePropName="checked">
+        <Checkbox>Remember me</Checkbox>
+      </Form.Item>
+
+      <Form.Item {...tailLayout}>
+        <Button type="primary" htmlType="submit">
+          Submit
+        </Button>
+      </Form.Item>
+      {/* <label>First Name</label>
       <input {...register('firstName')} />
       <label>Gender Selection</label>
       <select {...register('gender')}>
@@ -28,7 +76,7 @@ export function Form() {
           </option>
         ))}
       </select>
-      <input type="submit" />
-    </form>
+      <input type="submit" /> */}
+    </Form>
   )
 }
